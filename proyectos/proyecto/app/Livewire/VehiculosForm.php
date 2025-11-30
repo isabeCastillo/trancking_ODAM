deadedc8c810a07de6b5b8cab5d6bbeb1b4a74c7
DianaEspinal <dianicee2002@gmail.com>
2025-11-30 08:39:14 -0600

    Feat: Creando archivos de liveware sbre motoristas y vehiculos

---
+++ proyectos/proyecto/app/Livewire/VehiculosForm.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class VehiculosForm extends Component
+{
+    public function render()
+    {
+        return view('livewire.vehiculos-form');
+    }
+}
