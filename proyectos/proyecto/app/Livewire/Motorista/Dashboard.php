81eaf77242d9819f8a946fd2302d5c447c67f980
isabeCastillo <cp21026@ues.edu.sv>
2025-12-01 22:08:17 -0600

    fix: correcion de error en el admin para el livewire

---
+++ proyectos/proyecto/app/Livewire/Motorista/Dashboard.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Motorista;
+
+use Livewire\Component;
+
+class Dashboard extends Component
+{
+    public function render()
+    {
+        return view('livewire.motorista.dashboard');
+    }
+}
--- proyectos/proyecto/resources/views/components/layouts/admin.blade.php
+++ proyectos/proyecto/resources/views/components/layouts/admin.blade.php
@@ -321,14 +321,17 @@
                     @endauth
                 </div>
             </header>
-
             <main class="admin-content">
                 {{ $slot }}
             </main>
         </div>
     </div>
 
-    @livewireScripts
+    <script
+        src="{{ asset('vendor/livewire/livewire.js') }}"
+        data-csrf="{{ csrf_token() }}"
+        data-update-uri="/proyecto/public/livewire/update"
+        data-navigate-once="true">
+    </script>
 </body>
-
 </html>
